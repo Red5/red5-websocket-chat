8d5da97ccf602d2bcf53384b4931d51cc22655d8
Mondain <mondain@gmail.com>
2014-06-11 17:12:50 -0700

    Cleaned up

--- src/main/java/org/red5/demos/chat/Application.java
+++ src/main/java/org/red5/demos/chat/Application.java
@@ -1,16 +1,10 @@
 package org.red5.demos.chat;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.net.websocket.WebSocketPlugin;
 import org.red5.net.websocket.WebSocketScopeManager;
 import org.red5.server.adapter.MultiThreadedApplicationAdapter;
-import org.red5.server.api.IConnection;
-import org.red5.server.api.Red5;
 import org.red5.server.api.scope.IScope;
-import org.red5.server.api.stream.IBroadcastStream;
 import org.red5.server.plugin.PluginRegistry;
 import org.slf4j.Logger;
 import org.springframework.beans.BeansException;
@@ -24,8 +18,10 @@ import org.springframework.context.ApplicationContextAware;
  */
 public class Application extends MultiThreadedApplicationAdapter implements ApplicationContextAware {
 
+	@SuppressWarnings("unused")
 	private static Logger log = Red5LoggerFactory.getLogger(Application.class, "chat");
 
+	@SuppressWarnings("unused")
 	private ApplicationContext applicationContext;
 
 	@Override

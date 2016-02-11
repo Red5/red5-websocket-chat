433ec8a7ebcbb9cc3469d471249ad5854308ef89
Paul Gregoire <mondain@gmail.com>
2016-02-11 17:51:26 -0500

    Fixed

--- src/main/java/org/red5/demos/chat/Application.java
+++ src/main/java/org/red5/demos/chat/Application.java
@@ -18,32 +18,32 @@ import org.springframework.context.ApplicationContextAware;
  */
 public class Application extends MultiThreadedApplicationAdapter implements ApplicationContextAware {
 
-	private static Logger log = Red5LoggerFactory.getLogger(Application.class, "chat");
+    private static Logger log = Red5LoggerFactory.getLogger(Application.class, "chat");
 
-	@SuppressWarnings("unused")
-	private ApplicationContext applicationContext;
+    @SuppressWarnings("unused")
+    private ApplicationContext applicationContext;
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
-	@Override
-	public boolean appStart(IScope scope) {
-		log.info("Chat starting");
-		// add our application to enable websocket support
-		WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager();
-		manager.addApplication(scope);
-		return super.appStart(scope);
-	}
+    @Override
+    public boolean appStart(IScope scope) {
+        log.info("Chat starting");
+        // add our application to enable websocket support
+        WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager();
+        manager.addApplication(scope);
+        return super.appStart(scope);
+    }
 
-	@Override
-	public void appStop(IScope scope) {
-		log.info("Chat stopping");
-		// remove our app
-		WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager();
-		manager.removeApplication(scope);
-		super.appStop(scope);
-	}
+    @Override
+    public void appStop(IScope scope) {
+        log.info("Chat stopping");
+        // remove our app
+        WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager();
+        manager.removeApplication(scope);
+        super.appStop(scope);
+    }
 
 }
--- src/main/java/org/red5/demos/chat/WebSocketChatDataListener.java
+++ src/main/java/org/red5/demos/chat/WebSocketChatDataListener.java
@@ -24,106 +24,106 @@ import org.slf4j.Logger;
  */
 public class WebSocketChatDataListener extends WebSocketDataListener {
 
-	private static final Logger log = Red5LoggerFactory.getLogger(WebSocketChatDataListener.class, "chat");
+    private static final Logger log = Red5LoggerFactory.getLogger(WebSocketChatDataListener.class, "chat");
 
-	{
-		setProtocol("chat");
-	}
+    {
+        setProtocol("chat");
+    }
 
-	private Router router;
-	
-	private Set<WebSocketConnection> connections = new HashSet<WebSocketConnection>();
-	
-	@Override
-	public void onWSConnect(WebSocketConnection conn) {
-		log.info("Connect: {}", conn);
-		if (conn.getHeaders().containsKey(Constants.WS_HEADER_PROTOCOL)) {
-			String protocol = (String) conn.getHeaders().get(Constants.WS_HEADER_PROTOCOL);
-			if (protocol.indexOf("chat") != -1) {
-				log.debug("Chat enabled");
-			} else {
-				log.info("Chat is not in the connections protocol list");
-			}
-		}
-		connections.add(conn);
-	}
+    private Router router;
 
-	@Override
-	public void onWSDisconnect(WebSocketConnection conn) {
-		log.info("Disconnect: {}", conn);
-		connections.remove(conn);
-	}
+    private Set<WebSocketConnection> connections = new HashSet<WebSocketConnection>();
 
-	@Override
-	public void onWSMessage(WSMessage message) {
-		// if its protocol doesn't match then skip the message
-		if (!protocol.equals(message.getConnection().getProtocol())) {
-			log.debug("Skipping message due to protocol mismatch");
-			return;
-		}
-		// ignore ping and pong
-		if (message.getMessageType() == MessageType.PING || message.getMessageType() == MessageType.PONG) {
-			return;
-		}
-		// close if we get a close
-		if (message.getMessageType() == MessageType.CLOSE) {
-			message.getConnection().close();
-			return;
-		}
-		// get the connection path for routing
-		String path = message.getConnection().getPath();
-		log.debug("WebSocket connection path: {}", path);
-		// assume we have text
-		String msg = new String(message.getPayload().array()).trim();
-		log.info("onWSMessage: {}\n{}", msg, message.getConnection());
-		// do a quick hacky json check
-		if (msg.indexOf('{') != -1 && msg.indexOf(':') != -1) {
-			log.info("JSON encoded text message");
-			// channelName == roomid in most cases
-			JSONObject obj = null;
-			JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
-			try {
-				obj = (JSONObject) parser.parse(msg);
-				log.debug("Parsed - keys: {}\ncontent: {}", obj.keySet(), obj);
-				msg = JSONValue.toJSONString(obj);
-				// send to all websocket connections matching this connections path
-				sendToAll(path, msg);
-				// send to the shared object matching this connections path
-				router.route(path, msg);
-			} catch (ParseException e) {
-				log.warn("Exception parsing JSON", e);
-			}
-		} else {
-			log.info("Standard text message");
-			// send to all websocket connections matching this connections path
-			sendToAll(path, msg);
-			// send to the shared object matching this connections path
-			router.route(path, msg);
-		}
-	}
+    @Override
+    public void onWSConnect(WebSocketConnection conn) {
+        log.info("Connect: {}", conn);
+        if (conn.getHeaders().containsKey(Constants.WS_HEADER_PROTOCOL)) {
+            String protocol = (String) conn.getHeaders().get(Constants.WS_HEADER_PROTOCOL);
+            if (protocol.indexOf("chat") != -1) {
+                log.debug("Chat enabled");
+            } else {
+                log.info("Chat is not in the connections protocol list");
+            }
+        }
+        connections.add(conn);
+    }
 
-	/**
-	 * Send message to all connected connections.
-	 * 
-	 * @param path
-	 * @param msg
-	 */
-	public void sendToAll(String path, String message) {			
-		for (WebSocketConnection conn : connections) {
-			if (path.equals(conn.getPath())) {
-				try {
-					conn.send(message);
-				} catch (UnsupportedEncodingException e) {
-				}
-			} else {
-				log.trace("Path did not match for message {} != {}", path, conn.getPath());
-			}
-		}
-	}
+    @Override
+    public void onWSDisconnect(WebSocketConnection conn) {
+        log.info("Disconnect: {}", conn);
+        connections.remove(conn);
+    }
 
-	public void setRouter(Router router) {
-		this.router = router;
-		this.router.setWsListener(this);
-	}
+    @Override
+    public void onWSMessage(WSMessage message) {
+        // if its protocol doesn't match then skip the message
+        if (!protocol.equals(message.getConnection().getProtocol())) {
+            log.debug("Skipping message due to protocol mismatch");
+            return;
+        }
+        // ignore ping and pong
+        if (message.getMessageType() == MessageType.PING || message.getMessageType() == MessageType.PONG) {
+            return;
+        }
+        // close if we get a close
+        if (message.getMessageType() == MessageType.CLOSE) {
+            message.getConnection().close();
+            return;
+        }
+        // get the connection path for routing
+        String path = message.getConnection().getPath();
+        log.debug("WebSocket connection path: {}", path);
+        // assume we have text
+        String msg = new String(message.getPayload().array()).trim();
+        log.info("onWSMessage: {}\n{}", msg, message.getConnection());
+        // do a quick hacky json check
+        if (msg.indexOf('{') != -1 && msg.indexOf(':') != -1) {
+            log.info("JSON encoded text message");
+            // channelName == roomid in most cases
+            JSONObject obj = null;
+            JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
+            try {
+                obj = (JSONObject) parser.parse(msg);
+                log.debug("Parsed - keys: {}\ncontent: {}", obj.keySet(), obj);
+                msg = JSONValue.toJSONString(obj);
+                // send to all websocket connections matching this connections path
+                sendToAll(path, msg);
+                // send to the shared object matching this connections path
+                router.route(path, msg);
+            } catch (ParseException e) {
+                log.warn("Exception parsing JSON", e);
+            }
+        } else {
+            log.info("Standard text message");
+            // send to all websocket connections matching this connections path
+            sendToAll(path, msg);
+            // send to the shared object matching this connections path
+            router.route(path, msg);
+        }
+    }
+
+    /**
+     * Send message to all connected connections.
+     * 
+     * @param path
+     * @param msg
+     */
+    public void sendToAll(String path, String message) {
+        for (WebSocketConnection conn : connections) {
+            if (path.equals(conn.getPath())) {
+                try {
+                    conn.send(message);
+                } catch (UnsupportedEncodingException e) {
+                }
+            } else {
+                log.trace("Path did not match for message {} != {}", path, conn.getPath());
+            }
+        }
+    }
+
+    public void setRouter(Router router) {
+        this.router = router;
+        this.router.setWsListener(this);
+    }
 
 }
